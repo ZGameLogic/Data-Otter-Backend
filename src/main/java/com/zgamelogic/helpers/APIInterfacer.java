0f4ec2c2356471c13f1c4866215f8c7375c7f2c5
Ben Shabowski <benshabowski@gmail.com>
2023-06-27 12:05:44 -0500

    Adding time to complete calls to monitor

--- src/main/java/com/zgamelogic/helpers/APIInterfacer.java
+++ src/main/java/com/zgamelogic/helpers/APIInterfacer.java
@@ -8,14 +8,17 @@ import java.net.URI;
 public abstract class APIInterfacer {
 
     public static boolean pingAPI(APIMonitor apiMonitor){
+        apiMonitor.setCompletedInMilliseconds(System.currentTimeMillis());
         final String url = apiMonitor.getUrl()  + ":" + apiMonitor.getPort() + "/" + apiMonitor.getHealthCheckUrl();
         RestTemplate restTemplate = new RestTemplate();
         try {
             String response = restTemplate.getForObject(new URI(url), String.class);
             apiMonitor.setStatus(response.toLowerCase().contains("health"));
+            apiMonitor.setCompletedInMilliseconds(System.currentTimeMillis() - apiMonitor.getCompletedInMilliseconds());
             return true;
         } catch (Exception e) {
             apiMonitor.setStatus(false);
+            apiMonitor.setCompletedInMilliseconds(System.currentTimeMillis() - apiMonitor.getCompletedInMilliseconds());
             return false;
         }
     }
