ddc5db207cfd8fa6a63deb328b2405ada780e1d9
Ben Shabowski <benshabowski@gmail.com>
2023-08-08 16:16:12 -0500

    Adding endpoint to save token

---
+++ src/main/java/com/zgamelogic/data/serializable/iOSToken.java
@@ -0,0 +1,12 @@
+package com.zgamelogic.data.serializable;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@ToString
+public class iOSToken {
+    private String token;
+}
