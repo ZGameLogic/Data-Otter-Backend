27e3d6500ded188a5da05e4cc7980b3dae1128c1
Ben Shabowski <benshabowski@gmail.com>
2024-05-09 18:48:37 -0500

    More tests

--- src/test/java/com/zgamelogic/controllers/NodeControllerTest.java
+++ src/test/java/com/zgamelogic/controllers/NodeControllerTest.java
@@ -1,25 +1,41 @@
 package com.zgamelogic.controllers;
 
+import com.zgamelogic.data.nodeConfiguration.NodeConfiguration;
+import com.zgamelogic.data.nodeConfiguration.NodeConfigurationRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class NodeControllerTest {
 
+    @MockBean
+    private NodeConfigurationRepository nodeConfigurationRepository;
+
     @Autowired
     private MockMvc mockMvc;
 
+    @BeforeEach
+    void setUp() {
+        Mockito.when(nodeConfigurationRepository.save(Mockito.any())).thenReturn(new NodeConfiguration(1L, "Test"));
+    }
+
     @Test
-    void exampleEndpointTest() throws Exception {
-        mockMvc.perform(get("/monitors"))
-                .andExpect(status().isOk());
+    void registerNode() throws Exception {
+        mockMvc.perform(
+                post("/nodes").content("{\"name\":\"test\"}").contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(content().json(""));
     }
 }
