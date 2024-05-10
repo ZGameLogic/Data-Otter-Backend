d09c8e70b269f7a873cadb378ed91b4ad73e7068
Ben Shabowski <benshabowski@gmail.com>
2024-05-10 10:14:29 -0500

    More tests

--- src/test/java/com/zgamelogic/controllers/NodeControllerTest.java
+++ src/test/java/com/zgamelogic/controllers/NodeControllerTest.java
@@ -1,5 +1,6 @@
 package com.zgamelogic.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zgamelogic.data.nodeConfiguration.NodeConfiguration;
 import com.zgamelogic.data.nodeConfiguration.NodeConfigurationRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +13,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Optional;
-
 import static com.zgamelogic.data.Constants.MASTER_NODE_NAME;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -22,6 +21,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class NodeControllerTest {
+
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @MockBean
     private NodeConfigurationRepository nodeConfigurationRepository;
@@ -35,14 +36,15 @@ class NodeControllerTest {
     void setUp() {
         Mockito.when(masterNode.getName()).thenReturn(MASTER_NODE_NAME);
         Mockito.when(masterNode.getId()).thenReturn(1L);
-        Mockito.when(nodeConfigurationRepository.save(Mockito.any(NodeConfiguration.class))).thenReturn(new NodeConfiguration(2L, "test"));
     }
 
     @Test
     void registerNode() throws Exception {
-        NodeConfiguration test = new NodeConfiguration("test");
+        NodeConfiguration payload = new NodeConfiguration("test");
+        NodeConfiguration response = new NodeConfiguration(1L, "test");
+        Mockito.when(nodeConfigurationRepository.save(Mockito.any(NodeConfiguration.class))).thenReturn(response);
         mockMvc.perform(
-                post("/nodes").content(test).contentType(MediaType.APPLICATION_JSON)
+                post("/nodes").content(mapper.writeValueAsString(payload)).contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
