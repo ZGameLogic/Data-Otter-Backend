50d70b65584b24722f412dde868a5e5ce2785336
Ben Shabowski <benshabowski@gmail.com>
2024-12-30 10:25:35 -0600

    adding database stuff

--- src/main/java/com/zgamelogic/services/monitors/MonitorService.java
+++ src/main/java/com/zgamelogic/services/monitors/MonitorService.java
@@ -11,6 +11,8 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.net.Socket;
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
@@ -20,6 +22,34 @@ public class MonitorService {
 
     @Async("asyncExecutor")
     public CompletableFuture<MonitorStatusReport> getMonitorStatus(MonitorConfiguration monitorConfiguration) {
+        return switch(monitorConfiguration.getType()){
+            case API, WEB -> getMonitorWebStatus(monitorConfiguration);
+            case DATABASE -> getMonitorDatabaseStatus(monitorConfiguration);
+        };
+    }
+
+    private CompletableFuture<MonitorStatusReport> getMonitorDatabaseStatus(MonitorConfiguration monitorConfiguration) {
+        int attempts = 0;
+        long startTime = 0;
+        long endTime = 0;
+        String[] splitUrl = monitorConfiguration.getUrl().split(":");
+        String url = splitUrl[0];
+        String portString = splitUrl.length > 1 ? monitorConfiguration.getUrl().split(":")[1] : "1433";
+        int port = 1433;
+        try {
+            port = Integer.parseInt(portString);
+        } catch (NumberFormatException ignored) {}
+        while(attempts < 4) {
+            startTime = System.currentTimeMillis();
+            attempts++;
+            try (Socket socket = new Socket(url, port)) {
+                return CompletableFuture.completedFuture(new MonitorStatusReport(endTime - startTime, true, attempts, 0));
+            } catch (IOException ignored) {}
+        }
+        return CompletableFuture.completedFuture(new MonitorStatusReport(endTime - startTime, false, attempts, 0));
+    }
+
+    private CompletableFuture<MonitorStatusReport> getMonitorWebStatus(MonitorConfiguration monitorConfiguration) {
         RestTemplate restTemplate =  new RestTemplateBuilder().setConnectTimeout(Duration.ofSeconds(2)).setReadTimeout(Duration.ofSeconds(2)).build();
         HttpHeaders headers = new HttpHeaders();
         int attempts = 0;
