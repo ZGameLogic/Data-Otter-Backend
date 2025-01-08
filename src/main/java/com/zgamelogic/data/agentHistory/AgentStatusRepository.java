5733c9883110c90d59a9143e4dbc3c575c28b694
Ben Shabowski <benshabowski@gmail.com>
2025-01-08 16:50:38 -0600

    Better agent endpoints

--- src/main/java/com/zgamelogic/data/agentHistory/AgentStatusRepository.java
+++ src/main/java/com/zgamelogic/data/agentHistory/AgentStatusRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public interface AgentStatusRepository extends JpaRepository<AgentStatus, AgentStatus.AgentStatusId> {
     Optional<AgentStatus> findFirstByIdAgentIdAndIdDateAfterOrderByIdDateDesc(long id, Date date);
+
+    @Query("SELECT a FROM AgentStatus a WHERE a.id.agent.id = :agentId AND a.id.date BETWEEN :startDate AND :endDate ORDER BY a.id.date DESC")
+    List<AgentStatus> findByAgentIdAndDateBetween(long agentId, Date startDate, Date endDate);
 
     @Modifying
     @Transactional
