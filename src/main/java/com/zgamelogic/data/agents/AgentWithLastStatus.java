b35d0e879c9cd223768cc1918a056d285b8f0694
Ben Shabowski <benshabowski@gmail.com>
2025-01-08 12:42:21 -0600

    Better agent endpoints

--- src/main/java/com/zgamelogic/data/agents/AgentWithLastStatus.java
+++ src/main/java/com/zgamelogic/data/agents/AgentWithLastStatus.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.zgamelogic.data.agentHistory.AgentStatus;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 @JsonSerialize(using = AgentWithLastStatus.AgentWithLastStatusSerializer.class)
 public record AgentWithLastStatus(AgentStatus status, Agent agent) {
@@ -20,7 +21,14 @@ public record AgentWithLastStatus(AgentStatus status, Agent agent) {
             gen.writeStringField("os", value.agent.getOs());
             if(value.status != null) {
                 AgentStatus status = value.status;
-                gen.writeObjectField("status", status);
+                gen.writeObjectFieldStart("status");
+                String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(status.getId().getDate());
+                gen.writeStringField("date", date);
+                gen.writeNumberField("memory usage", status.getMemoryUsage());
+                gen.writeNumberField("cpu usage", status.getCpuUsage());
+                gen.writeNumberField("disk usage", status.getDiskUsage());
+                gen.writeStringField("agent version", status.getAgentVersion());
+                gen.writeEndObject();
             }
             gen.writeEndObject();
         }
