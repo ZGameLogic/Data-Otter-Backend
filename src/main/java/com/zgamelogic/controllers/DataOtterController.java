5e9d7d47cfc01da4a780e7c677a3592a5cc398a2
Ben Shabowski <benshabowski@gmail.com>
2025-01-15 08:39:13 -0600

    removing no longer needed code

--- src/main/java/com/zgamelogic/controllers/DataOtterController.java
+++ src/main/java/com/zgamelogic/controllers/DataOtterController.java
@@ -70,7 +70,7 @@ public class DataOtterController {
     @Scheduled(cron = "0 * * * * *")
     public void cleanup(){
         Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_YEAR, -1);
+        calendar.add(Calendar.HOUR, -12);
         Date oneWeekAgo = calendar.getTime();
         monitorStatusRepository.deleteRecordsOlderThan(oneWeekAgo);
         agentStatusRepository.deleteRecordsOlderThan(oneWeekAgo);
